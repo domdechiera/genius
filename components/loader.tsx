a738f94b170be31c27c0cb5177a0a117002084c7
DomDeChiera <dom@dechiera.com>
2024-10-16 16:08:03 +0800

    Rebrand to og Genius

--- components/loader.tsx
+++ components/loader.tsx
@@ -11,7 +11,7 @@ export const Loader = () => {
                 />
             </div>
             <p className="text-sm text-muted-foreground">
-                Onux is thinking...
+                Genius is thinking...
             </p>
         </div>
     );
